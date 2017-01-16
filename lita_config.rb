c3327af52fb0fc06841db57bdd4ad145ff7c6d9b
Frank <myself162@hotmail.com>
2017-01-16 16:04:17 +0800

    remove redistogo url

--- lita_config.rb
+++ lita_config.rb
@@ -11,8 +11,8 @@ Lita.configure do |config|
   config.robot.log_level = :info
   config.robot.adapter = :slack
   config.adapters.slack.token = "xoxb-126406817969-4zuLHBcbiT5xKkip5nEmw2kp"
-  config.redis[:url] = ENV["REDISTOGO_URL"] = 'redis://redistogo:98589b605b2aa58e2c2afac974448273@viperfish.redistogo.com:11759/'
-  # config.http.port = ENV["PORT"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["PORT"]
 
   # An array of user IDs that are considered administrators. These users
   # the ability to add and remove other users from authorization groups.
